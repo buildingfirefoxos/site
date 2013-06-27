46567d2c2a30d672cc36ca9df596dbe901eb84c1
rnowm <arnau@arnaumarch.com>
2013-06-27 13:00:56 +0200

    fixed hover for buttons BB examples

--- js/site.js
+++ js/site.js
@@ -33,6 +33,8 @@ $(function() {
   });
 
   //Detect Nightly browser
+        $('body').removeClass('image-fallback');
+
   if (/Firefox[\/\s](\d+\.\d+)/.test(navigator.userAgent)){ //test for Firefox/x.x or Firefox x.x (ignoring remaining digits);
     var ffversion=new Number(RegExp.$1) // capture x.x portion and store as a number
     if (ffversion>=20) {
